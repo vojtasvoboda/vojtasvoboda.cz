778d11fea291d9409781e2b34aa4353be2422af9
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2017-02-15 17:13:28 +0100

    Setup bootstrap theme

--- themes/vojtasvoboda/assets/javascript/site.js
+++ themes/vojtasvoboda/assets/javascript/site.js
@@ -1,3 +1,5 @@
-/*! vojtasvoboda.cz-theme - v1.0.0 - 2015-08-29
-* Copyright (c) 2015 ; Licensed  */
-var main=function(){};$(document).ready(main);
+var application = function($) {
+    // place for your code
+};
+
+jQuery(document).ready(application);
