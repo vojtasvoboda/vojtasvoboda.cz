d0ae4ea651cf3aa0c4baf5d532fab1593c67fb5c
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2017-02-18 08:52:14 +0100

    Update CMS

--- modules/system/assets/js/framework.js
+++ modules/system/assets/js/framework.js
@@ -75,6 +75,7 @@ if (window.jQuery.request !== undefined) {
 
         var requestOptions = {
             url: window.location.href,
+            crossDomain: false,
             context: context,
             headers: requestHeaders,
             success: function(data, textStatus, jqXHR) {
